dd6f35180e1e1bb19342a5c3765b920642749fb7
Antonio Sanchez <antonio.sanchez.leon@outlook.com>
2022-05-20 23:02:30 +0300

    Implement CSV load function

--- src/etl/etl.cpp
+++ src/etl/etl.cpp
@@ -1,6 +1,19 @@
 #include "etl.hpp"
 
+#include "csv.hpp"
+
 std::vector<std::vector<double>> etl::load_from_csv(
     std::istringstream &stream) {
-  return {};
+  std::vector<std::vector<double>> output;
+  csv::CSVFormat format;
+  format.no_header();
+  csv::CSVReader reader(stream, format);
+  for (csv::CSVRow &row : reader) {
+    std::vector<double> output_row;
+    for (csv::CSVField &field : row) {
+      output_row.push_back(field.get<double>());
+    }
+    output.push_back(output_row);
+  }
+  return output;
 }
