d8e3518fae15365f2dbdd7c0ac31e29239a7beda
Antonio Sanchez <antonio.sanchez.leon@outlook.com>
2022-05-20 22:53:32 +0300

    Fix raw string in unit test

--- tests/test_etl.cpp
+++ tests/test_etl.cpp
@@ -5,10 +5,8 @@
 #include "gtest/gtest.h"
 
 TEST(etl, load_from_csv) {
-  std::istringstream input{R"(
-1.0,1.1
-2.0,2.1
-  )"};
+  std::istringstream input{R"(1.0,1.1
+2.0,2.1)"};
   const std::vector<std::vector<double>> expected{{1.0, 1.1}, {2.0, 2.1}};
   auto observed = etl::load_from_csv(input);
   EXPECT_EQ(expected, observed);
