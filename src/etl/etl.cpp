bd8892bd380caef90c56011f63f50dc975c14a39
Antonio Sanchez <antonio.sanchez.leon@outlook.com>
2022-05-22 20:28:40 +0300

    Move load routine to a function

--- src/etl/etl.cpp
+++ src/etl/etl.cpp
@@ -2,12 +2,8 @@
 
 #include "csv.hpp"
 
-std::vector<std::vector<double>> etl::load_from_csv(
-    std::istringstream &stream) {
+static std::vector<std::vector<double>> load(csv::CSVReader &reader) {
   std::vector<std::vector<double>> output;
-  csv::CSVFormat format;
-  format.no_header();
-  csv::CSVReader reader(stream, format);
   for (csv::CSVRow &row : reader) {
     std::vector<double> output_row;
     for (csv::CSVField &field : row) {
@@ -16,4 +12,12 @@ std::vector<std::vector<double>> etl::load_from_csv(
     output.push_back(output_row);
   }
   return output;
+}
+
+std::vector<std::vector<double>> etl::load_from_csv(
+    std::istringstream &stream) {
+  csv::CSVFormat format;
+  format.no_header();
+  csv::CSVReader reader(stream, format);
+  return load(reader);
 }
